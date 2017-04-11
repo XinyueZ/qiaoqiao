53b7ac8e144f9083cc17de9dd913ef1e56e17f65
Chris Zhao <xinyue.zhao@immonet.de>
2017-04-11 14:47:09 +0200

    Added click-listeners for main-control

--- app/src/main/java/com/qiaoqiao/views/MainControl.java
+++ app/src/main/java/com/qiaoqiao/views/MainControl.java
@@ -3,14 +3,28 @@ package com.qiaoqiao.views;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import com.qiaoqiao.databinding.LayoutMainControlBinding;
 
+import java.lang.ref.WeakReference;
+
 public final class MainControl extends FrameLayout {
 	private LayoutMainControlBinding mBinding;
+	private WeakReference<OnFromLocalClickedListener> mOnFromLocalClickedListenerWeakReference;
+	private WeakReference<OnCaptureClickedListener> mOnCaptureClickedListenerWeakReference;
+	private WeakReference<OnFromWebClickedListener> mOnFromWebClickedListenerWeakReference;
+
+	public interface OnFromLocalClickedListener extends OnClickListener {}
+
+	public interface OnCaptureClickedListener extends OnClickListener {}
+
+	public interface OnFromWebClickedListener extends OnClickListener {}
+
 
 	public MainControl(Context context) {
 		super(context);
@@ -30,5 +44,44 @@ public final class MainControl extends FrameLayout {
 	private void init(@NonNull Context cxt) {
 		LayoutInflater inflater = LayoutInflater.from(cxt);
 		mBinding = LayoutMainControlBinding.inflate(inflater, this, true);
+	}
+
+	public void setOnFromLocalClickedListener(@Nullable OnFromLocalClickedListener l) {
+		mOnFromLocalClickedListenerWeakReference = new WeakReference<>(l);
+		mBinding.fromLocalFb.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (mOnFromLocalClickedListenerWeakReference.get() != null) {
+					mOnFromLocalClickedListenerWeakReference.get()
+					                                        .onClick(v);
+				}
+			}
+		});
+	}
+
+	public void setOnCaptureClickedListener(@Nullable OnCaptureClickedListener l) {
+		mOnCaptureClickedListenerWeakReference = new WeakReference<>(l);
+		mBinding.captureFb.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (mOnCaptureClickedListenerWeakReference.get() != null) {
+					mOnCaptureClickedListenerWeakReference.get()
+					                                      .onClick(v);
+				}
+			}
+		});
+	}
+
+	public void setOnFromWebClickedListener(@Nullable OnFromWebClickedListener l) {
+		mOnFromWebClickedListenerWeakReference = new WeakReference<>(l);
+		mBinding.fromWebFb.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (mOnFromWebClickedListenerWeakReference.get() != null) {
+					mOnFromWebClickedListenerWeakReference.get()
+					                                      .onClick(v);
+				}
+			}
+		});
 	}
 }
