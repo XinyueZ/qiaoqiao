282c477111cca85515540042e203b9f55004b8e1
Chris Zhao <xinyue.zhao@immonet.de>
2017-05-11 12:44:45 +0200

    Added jwiki for ready for sdk loading on wiki, use planttext, wikistyle

--- app/src/main/java/com/qiaoqiao/backend/Wikipedia.java
+++ app/src/main/java/com/qiaoqiao/backend/Wikipedia.java
@@ -14,6 +14,7 @@ public interface Wikipedia {
 	                                 @Query("prop") String prop,
 	                                 @Query("piprop") String piprop,
 	                                 @Query("explaintext") boolean explaintext,
+	                                 @Query("exsectionformat") String exsectionformat,
 	                                 @Query("exlimit") int exlimit,
 	                                 @Query("redirects") String redirects,
 	                                 @Query("titles") String titles);
