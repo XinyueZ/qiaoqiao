d248f5b5dbc490b91c8dc18edd11907a1cf98916
Chris Zhao <xinyue.zhao@immonet.de>
2017-05-17 23:10:28 +0200

    Used vision-id for transition with description

--- app/src/main/java/com/qiaoqiao/vision/ui/viewholder/AbstractVisionViewHolder.java
+++ app/src/main/java/com/qiaoqiao/vision/ui/viewholder/AbstractVisionViewHolder.java
@@ -1,16 +1,20 @@
 package com.qiaoqiao.vision.ui.viewholder;
 
+import android.content.Context;
 import android.databinding.ViewDataBinding;
 import android.support.annotation.NonNull;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.qiaoqiao.R;
 import com.qiaoqiao.vision.bus.VisionEntityClickEvent;
 import com.qiaoqiao.vision.model.VisionEntity;
 
 import java.util.List;
+import java.util.Locale;
 
 import de.greenrobot.event.EventBus;
 
@@ -26,13 +30,29 @@ public abstract class AbstractVisionViewHolder extends RecyclerView.ViewHolder {
 		mBinding = binding;
 	}
 
+	protected static void loadImage(Context cxt, VisionEntity entity, ImageView imageView) {
+		String baseUrl = cxt.getString(R.string.base_url_knowledge);
+		String imageUrl = String.format(baseUrl + "images/wikipedia?language=%s&keyword=%s",
+		                                Locale.getDefault()
+		                                      .getLanguage(),
+		                                entity.getDescription()
+		                                      .getDescriptionText());
+		Glide.with(cxt)
+		     .load(imageUrl)
+		     .centerCrop()
+		     .placeholder(R.drawable.ic_default_image)
+		     .error(R.drawable.ic_default_image)
+		     .crossFade()
+		     .into(imageView);
+	}
+
 	public void onClicked(VisionEntity visionEntity) {
 		mVisionEntityClickEvent.setEntity(visionEntity);
 
 
 		ViewCompat.setTransitionName(getTransitionView(),
 		                             itemView.getContext()
-		                                     .getString(R.string.transition_share_item_name) + "-" + visionEntity.getDescription());
+		                                     .getString(R.string.transition_share_item_name) + "-" + visionEntity.getId() + "-" + visionEntity.getDescription());
 		mVisionEntityClickEvent.setTransitionView(getTransitionView());
 		EventBus.getDefault()
 		        .post(mVisionEntityClickEvent);
--- app/src/main/java/com/qiaoqiao/vision/ui/viewholder/WebCellViewHolder.java
+++ app/src/main/java/com/qiaoqiao/vision/ui/viewholder/WebCellViewHolder.java
@@ -1,17 +1,12 @@
 package com.qiaoqiao.vision.ui.viewholder;
 
-import android.content.Context;
 import android.support.annotation.NonNull;
 import android.view.View;
-import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.qiaoqiao.R;
 import com.qiaoqiao.databinding.WebCellViewBinding;
 import com.qiaoqiao.vision.model.VisionEntity;
 
 import java.util.List;
-import java.util.Locale;
 
 
 public final class WebCellViewHolder extends AbstractVisionViewHolder {
@@ -24,22 +19,6 @@ public final class WebCellViewHolder extends AbstractVisionViewHolder {
 		mSize = size;
 	}
 
-
-	private static void loadImage(Context cxt, VisionEntity entity, ImageView imageView) {
-		String baseUrl = cxt.getString(R.string.base_url_knowledge);
-		String imageUrl = String.format(baseUrl + "images/wikipedia?language=%s&keyword=%s",
-		                                Locale.getDefault()
-		                                      .getLanguage(),
-		                                entity.getDescription()
-		                                      .getDescriptionText());
-		Glide.with(cxt)
-		     .load(imageUrl)
-		     .centerCrop()
-		     .placeholder(R.drawable.ic_default_image)
-		     .error(R.drawable.ic_default_image)
-		     .crossFade()
-		     .into(imageView);
-	}
 
 	@Override
 	public void onBindViewHolder() {
