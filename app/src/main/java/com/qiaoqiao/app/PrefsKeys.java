b5fd9957a186589f325c02fd64b58df31ae660fa
Chris Zhao <xinyue.zhao@immonet.de>
2017-05-31 11:34:34 +0200

    Can save confidence

--- app/src/main/java/com/qiaoqiao/app/PrefsKeys.java
+++ app/src/main/java/com/qiaoqiao/app/PrefsKeys.java
@@ -4,4 +4,8 @@ public interface PrefsKeys {
 	String KEY_GOOGLE_ID = "googleId";
 	String KEY_GOOGLE_PHOTO_URL = "googlePhotoUrl";
 	String KEY_GOOGLE_DISPLAY_NAME = "googleDisplayName";
+
+	String KEY_CONFIDENCE_LABEL = "confidenceLabel";
+	String KEY_CONFIDENCE_LOGO = "confidenceLogo";
+	String KEY_CONFIDENCE_IMAGE = "confidenceImage";
 }
--- app/src/main/java/com/qiaoqiao/core/confidence/ui/ConfidenceDialogFragment.java
+++ app/src/main/java/com/qiaoqiao/core/confidence/ui/ConfidenceDialogFragment.java
@@ -2,13 +2,17 @@ package com.qiaoqiao.core.confidence.ui;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Px;
+import android.support.v4.content.SharedPreferencesCompat;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.support.v7.preference.PreferenceManager;
 import android.widget.SeekBar;
 
 import com.amulyakhare.textdrawable.TextDrawable;
@@ -17,9 +21,14 @@ import com.qiaoqiao.core.confidence.ConfidenceContract;
 import com.qiaoqiao.core.confidence.ConfidencePresenter;
 import com.qiaoqiao.databinding.FragmentConfidenceDialogBinding;
 
+import static com.qiaoqiao.app.PrefsKeys.KEY_CONFIDENCE_IMAGE;
+import static com.qiaoqiao.app.PrefsKeys.KEY_CONFIDENCE_LABEL;
+import static com.qiaoqiao.app.PrefsKeys.KEY_CONFIDENCE_LOGO;
+
 
 public final class ConfidenceDialogFragment extends AppCompatDialogFragment implements ConfidenceContract.View,
-                                                                                       SeekBar.OnSeekBarChangeListener {
+                                                                                       SeekBar.OnSeekBarChangeListener,
+                                                                                       DialogInterface.OnClickListener {
 	private ConfidenceContract.Presenter mPresenter;
 	private FragmentConfidenceDialogBinding mBinding;
 	private @Px static final int W = 95;
@@ -35,16 +44,46 @@ public final class ConfidenceDialogFragment extends AppCompatDialogFragment impl
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		mBinding = FragmentConfidenceDialogBinding.inflate(getActivity().getLayoutInflater());
 		mBinding.setSeekBarChangeHandler(this);
-		mBinding.setThumbLabel(createThumbDrawable(0));
-		mBinding.setThumbLogo(createThumbDrawable(0));
-		mBinding.setThumbImage(createThumbDrawable(0));
+		final float cfLabel = readPrefs(KEY_CONFIDENCE_LABEL);
+		final float cfLogo = readPrefs(KEY_CONFIDENCE_LOGO);
+		final float cfImage = readPrefs(KEY_CONFIDENCE_IMAGE);
+		mBinding.setThumbLabel(createThumbDrawable(cfLabel));
+		mBinding.setThumbLogo(createThumbDrawable(cfLogo));
+		mBinding.setThumbImage(createThumbDrawable(cfImage));
+		mBinding.setCfLabel((int) ((cfLabel) * 100));
+		mBinding.setCfLogo((int) ((cfLogo) * 100));
+		mBinding.setCfImage((int) ((cfImage) * 100));
 		builder.setView(mBinding.getRoot())
 		       .setTitle(R.string.confidence_title)
-		       .setPositiveButton(android.R.string.ok, null);
+		       .setPositiveButton(android.R.string.ok, this)
+		       .setNegativeButton(android.R.string.cancel, null);
 		// Create the AlertDialog object and return it
 		return builder.create();
 	}
 
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		writePrefs();
+	}
+
+	private void writePrefs() {
+		writePrefs(KEY_CONFIDENCE_LABEL, mBinding.confidenceLabelValue.getProgress());
+		writePrefs(KEY_CONFIDENCE_LOGO, mBinding.confidenceLogoValue.getProgress());
+		writePrefs(KEY_CONFIDENCE_IMAGE, mBinding.confidenceImageValue.getProgress());
+	}
+
+	private void writePrefs(@NonNull String key, int value) {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+		final SharedPreferences.Editor edit = prefs.edit();
+		edit.putFloat(key, value / 100.f);
+		SharedPreferencesCompat.EditorCompat.getInstance()
+		                                    .apply(edit);
+	}
+
+	private float readPrefs(@NonNull String key) {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+		return prefs.getFloat(key, 0.f);
+	}
 
 	@Override
 	public void setPresenter(@NonNull ConfidencePresenter presenter) {
