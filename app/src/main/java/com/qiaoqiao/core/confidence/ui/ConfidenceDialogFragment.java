b1ef3009bd0c24ebc90daf94b8337bf627dbbfc1
Chris Zhao <xinyue.zhao@immonet.de>
2017-05-31 10:54:51 +0200

    Optimized text-drawable usage on seekbar of confidence

--- app/src/main/java/com/qiaoqiao/core/confidence/ui/ConfidenceDialogFragment.java
+++ app/src/main/java/com/qiaoqiao/core/confidence/ui/ConfidenceDialogFragment.java
@@ -2,13 +2,10 @@ package com.qiaoqiao.core.confidence.ui;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Px;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
@@ -25,6 +22,8 @@ public final class ConfidenceDialogFragment extends AppCompatDialogFragment impl
                                                                                        SeekBar.OnSeekBarChangeListener {
 	private ConfidenceContract.Presenter mPresenter;
 	private FragmentConfidenceDialogBinding mBinding;
+	private @Px static final int W = 95;
+	private @Px static final int H = 95;
 
 	public static ConfidenceDialogFragment newInstance(@NonNull Context cxt) {
 		return (ConfidenceDialogFragment) ConfidenceDialogFragment.instantiate(cxt, ConfidenceDialogFragment.class.getName());
@@ -36,25 +35,9 @@ public final class ConfidenceDialogFragment extends AppCompatDialogFragment impl
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		mBinding = FragmentConfidenceDialogBinding.inflate(getActivity().getLayoutInflater());
 		mBinding.setSeekBarChangeHandler(this);
-		mBinding.setThumbLabel(TextDrawable.builder()
-		                                   .beginConfig()
-		                                   .width(80)  // width in px
-		                                   .height(80) // height in px
-		                                   .endConfig()
-		                                   .buildRound("0", ResourcesCompat.getColor(getResources(), R.color.colorPrimaryDark, null)));
-		mBinding.setThumbLogo(TextDrawable.builder()
-		                                  .beginConfig()
-		                                  .width(80)  // width in px
-		                                  .height(80) // height in px
-		                                  .endConfig()
-		                                  .buildRound("0", ResourcesCompat.getColor(getResources(), R.color.colorPrimaryDark, null)));
-		mBinding.setThumbImage(TextDrawable.builder()
-		                                   .beginConfig()
-		                                   .width(80)  // width in px
-		                                   .height(80) // height in px
-		                                   .endConfig()
-		                                   .buildRound("0", ResourcesCompat.getColor(getResources(), R.color.colorPrimaryDark, null)));
-
+		mBinding.setThumbLabel(createThumbDrawable(0));
+		mBinding.setThumbLogo(createThumbDrawable(0));
+		mBinding.setThumbImage(createThumbDrawable(0));
 		builder.setView(mBinding.getRoot())
 		       .setTitle(R.string.confidence_title)
 		       .setPositiveButton(android.R.string.ok, null);
@@ -62,19 +45,6 @@ public final class ConfidenceDialogFragment extends AppCompatDialogFragment impl
 		return builder.create();
 	}
 
-	public BitmapDrawable writeOnDrawable(Bitmap thumb, String text) {
-
-		Bitmap bm = thumb.copy(Bitmap.Config.ARGB_8888, true);
-
-		Paint paint = new Paint();
-		paint.setStyle(Paint.Style.FILL);
-		paint.setColor(Color.parseColor("#288ca0"));
-		paint.setTextSize(bm.getHeight() / 3);
-		Canvas canvas = new Canvas(bm);
-		canvas.drawText(text, bm.getWidth() / 4 - bm.getWidth() / 10, bm.getHeight() / 2 + bm.getHeight() / 10, paint);
-
-		return new BitmapDrawable(getResources(), bm);
-	}
 
 	@Override
 	public void setPresenter(@NonNull ConfidencePresenter presenter) {
@@ -90,13 +60,19 @@ public final class ConfidenceDialogFragment extends AppCompatDialogFragment impl
 	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 //		TextDrawable textDrawable = (TextDrawable) seekBar.getThumb();
 //		textDrawable.
-		seekBar.setThumb(TextDrawable.builder()
-		                             .beginConfig()
-		                             .width(80)  // width in px
-		                             .height(80) // height in px
-		                             .endConfig()
-		                             .buildRound(String.valueOf(progress), ResourcesCompat.getColor(getResources(), R.color.colorPrimaryDark, null)));
+		seekBar.setThumb(createThumbDrawable(progress / 100.f));
 
+	}
+
+	private TextDrawable createThumbDrawable(float progress) {
+		final Resources resources = getResources();
+		return TextDrawable.builder()
+		                   .beginConfig()
+		                   .width(W)
+		                   .height(H)
+		                   .textColor(ResourcesCompat.getColor(resources, R.color.colorYellow, null))
+		                   .endConfig()
+		                   .buildRound(String.valueOf(progress), ResourcesCompat.getColor(resources, R.color.colorPrimaryDark, null));
 	}
 
 	@Override
