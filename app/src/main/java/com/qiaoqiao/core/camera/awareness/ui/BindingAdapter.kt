6ce725ccfd0c4a4aefac97606086f928800df63b
Chris Zhao <xinyue.zhao@immonet.de>
2017-06-07 09:12:40 +0800

    Fixed crash for adjust showing on pb

--- app/src/main/java/com/qiaoqiao/core/camera/awareness/ui/BindingAdapter.kt
+++ app/src/main/java/com/qiaoqiao/core/camera/awareness/ui/BindingAdapter.kt
@@ -5,13 +5,15 @@ import android.widget.SeekBar
 import com.qiaoqiao.core.camera.awareness.map.PlaceWrapper
 
 @android.databinding.BindingAdapter("bind:adjust")
-fun setAdjustSeekBar(seekBar: SeekBar, adjust: Adjust) {
+fun setAdjustSeekBar(seekBar: SeekBar, adjust: Adjust?) {
+    if(adjust == null) return
+
     if (adjust.value == 0) {
         adjust.onProgressChanged(seekBar, 0, false)
     }
 
     seekBar.setOnSeekBarChangeListener(adjust)
-    seekBar.progress = adjust.value
+    seekBar.progress = adjust?.value
 }
 
 @android.databinding.BindingAdapter("bind:placeWrapper")
