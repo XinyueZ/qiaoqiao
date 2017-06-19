4fe0232d5d19dee2b5bfd53e7ec85c5302998173
Chris Zhao <xinyue.zhao@immonet.de>
2017-06-19 14:09:47 +0800

    Added get image in uri from stackview

--- app/src/main/java/com/qiaoqiao/core/camera/history/HistoryPresenter2.java
+++ app/src/main/java/com/qiaoqiao/core/camera/history/HistoryPresenter2.java
@@ -1,13 +1,22 @@
 package com.qiaoqiao.core.camera.history;
 
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.qiaoqiao.core.camera.crop.CropPresenter;
 import com.qiaoqiao.core.camera.history.bus.HistoryItemClickEvent;
 import com.qiaoqiao.repository.database.HistoryItem;
+import com.qiaoqiao.utils.ImageUtils;
+
+import java.lang.ref.WeakReference;
 
 import javax.inject.Inject;
 
@@ -21,21 +30,40 @@ public final class HistoryPresenter2 implements HistoryContract.Presenter2 {
 	private final @NonNull HistoryContract.View2 mView;
 	private @Nullable RealmResults<HistoryItem> mResult;
 	private @Nullable CropPresenter mCropPresenter;
+	private WeakReference<Context> mContextRef;
 
 	@Subscribe
 	public void onEvent(HistoryItemClickEvent e) {
 		if (mCropPresenter == null) {
 			return;
 		}
-		if (e.getHistoryItem() != null) {
-			mCropPresenter.openCrop(e.getHistoryItem()
-			                         .getByteArray());
+		final HistoryItem historyItem = e.getHistoryItem();
+		if (historyItem != null) {
+			if (historyItem.getByteArray() != null && historyItem.getByteArray().length != 0) {
+				mCropPresenter.openCrop(historyItem.getByteArray());
+			} else {
+				if (mContextRef.get() == null) {
+					return;
+				}
+				Glide.with(mContextRef.get())
+				     .load(historyItem.getImageUri())
+				     .asBitmap()
+				     .diskCacheStrategy(DiskCacheStrategy.ALL)
+				     .skipMemoryCache(false)
+				     .into(new SimpleTarget<Bitmap>() {
+					     @Override
+					     public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+						     mCropPresenter.openCrop(ImageUtils.convertImage2Bytes(resource));
+					     }
+				     });
+			}
 		}
 	}
 
 	@Inject
-	HistoryPresenter2(@NonNull HistoryContract.View2 view) {
+	HistoryPresenter2(@NonNull Context context, @NonNull HistoryContract.View2 view) {
 		mView = view;
+		mContextRef = new WeakReference<>(context);
 	}
 
 	@Inject
