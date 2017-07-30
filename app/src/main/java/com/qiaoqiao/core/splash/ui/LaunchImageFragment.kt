d47e94004cb1bd496c6243288bd4687d1b61fcc9
Chris Zhao <xinyue.zhao@immonet.de>
2017-07-30 17:10:01 +0200

    Fixed opening camera-activity

--- app/src/main/java/com/qiaoqiao/core/splash/ui/LaunchImageFragment.kt
+++ app/src/main/java/com/qiaoqiao/core/splash/ui/LaunchImageFragment.kt
@@ -101,7 +101,7 @@ class LaunchImageFragment : Fragment(), SplashContract.LaunchImageView, RequestL
     }
 
     private fun goToHome() {
-        CameraActivity.showInstance(activity, true)
+        CameraActivity.showInstance(activity)
         activity.finish()
     }
 }
