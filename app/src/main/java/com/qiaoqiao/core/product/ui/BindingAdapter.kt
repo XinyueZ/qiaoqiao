293d619aa0a95189d4a7a12337d9ccdae55d0df8
Chris Zhao <xinyue.zhao@immonet.de>
2017-08-14 22:31:58 +0200

    Added autoText

--- app/src/main/java/com/qiaoqiao/core/product/ui/BindingAdapter.kt
+++ app/src/main/java/com/qiaoqiao/core/product/ui/BindingAdapter.kt
@@ -1,9 +1,24 @@
 package com.qiaoqiao.core.product.ui
 
+import android.text.TextUtils
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import com.qiaoqiao.repository.backend.model.product.Product
 
 @android.databinding.BindingAdapter("bind:product")
 fun setProductOnRecyclerView(imageView: ImageView, product: Product) {
 }
+
+@android.databinding.BindingAdapter("bind:autoText")
+fun setAutoText(textView: TextView, someText: String) {
+    when {
+        TextUtils.isEmpty(someText) || TextUtils.isEmpty(someText.trim()) -> textView.visibility = View.GONE
+        else -> {
+            textView.visibility = View.VISIBLE
+            textView.text = someText
+        }
+    }
+}
+
 
