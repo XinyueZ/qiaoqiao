f21b5e39f3e2ad8b9f5fc1445d81ec26e25253aa
Chris Zhao <xinyue.zhao@immonet.de>
2017-08-21 09:55:59 +0200

    Added vibration feedback

--- app/src/main/java/com/qiaoqiao/core/camera/ui/CameraSetup.kt
+++ app/src/main/java/com/qiaoqiao/core/camera/ui/CameraSetup.kt
@@ -75,6 +75,7 @@ internal object CameraSetup {
                     })
             }
             mBinding.flashFab.setOnClickListener {
+                vibrator.vibrate(PrefsKeys.VIB_LNG)
                 mBinding.cameraSource?.let {
                     var mode = if (TextUtils.equals(it.flashMode, Camera.Parameters.FLASH_MODE_ON))
                         Camera.Parameters.FLASH_MODE_OFF else Camera.Parameters.FLASH_MODE_ON
@@ -82,6 +83,10 @@ internal object CameraSetup {
                         mBinding.flashFab.setImageResource(if (TextUtils.equals(it.flashMode, Camera.Parameters.FLASH_MODE_OFF))
                             R.drawable.ic_flash_on else R.drawable.ic_flash_off)
                 }
+            }
+
+            mBinding.cameraFaceFab.setOnClickListener {
+                vibrator.vibrate(PrefsKeys.VIB_LNG)
             }
         }
     }
